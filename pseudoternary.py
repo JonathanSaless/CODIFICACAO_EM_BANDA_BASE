23b5db76b697a63030248e87750e5d527a610e43
JonathanSaless <51421355+JonathanSaless@users.noreply.github.com>
2022-05-19 10:20:53 -0300

    Update pseudoternary.py

--- pseudoternary.py
+++ pseudoternary.py
@@ -1,13 +1,12 @@
 import numpy as np
 
-#PSEUDOTERNARY
 def PSEUDOTERNARY(dados):
-    pseudoternary = []
-    ultimo_volt = -1
-    for i in range (len(dados)):
-        if dados[i] == 1:
-            pseudoternary.append(0)
-        else:
-            ultimo_volt *= -1
-            pseudoternary.append(ultimo_volt)
-    return pseudoternary
+  pseudoternary = []
+  ultimo_volt = -1
+  for i in range (len(dados)):
+    if dados[i] == 0:
+      ultimo_volt *= -1
+      pseudoternary.append(ultimo_volt)
+    else:
+      pseudoternary.append(0)
+  return pseudoternary
