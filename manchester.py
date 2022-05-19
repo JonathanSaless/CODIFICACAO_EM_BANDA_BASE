270ade32474449e3e4582a740489f5c518391d59
JonathanSaless <51421355+JonathanSaless@users.noreply.github.com>
2022-05-19 10:18:17 -0300

    Update manchester.py

--- manchester.py
+++ manchester.py
@@ -1,13 +1,12 @@
 import numpy as np
 
-#MANCHESTER
-def MANCHESTER(dados):
-    manchester = []
-    for i in range(len(dados)):
-      if dados[i] == 0:
-        manchester.append(1)
-        manchester.append(-1)
-      else:
-        manchester.append(-1)
-        manchester.append(1) 
-    return manchester
+def MANCHESTER(dados): 
+  manchester = []
+  for i in range(len(dados)):
+    if dados[i] == 0:
+      manchester.append(1)
+      manchester.append(-1)
+    else:
+      manchester.append(-1)
+      manchester.append(1)
+  return manchester
