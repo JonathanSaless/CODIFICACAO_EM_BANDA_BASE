16c0239e8f80480ce54ac34f67d6dc6376fb7167
JonathanSaless <51421355+JonathanSaless@users.noreply.github.com>
2022-05-19 10:19:33 -0300

    Update mostrar_grafico.py

--- mostrar_grafico.py
+++ mostrar_grafico.py
@@ -1,33 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def mostrar_grafico(dados, dados_saida, titulo, cor):
-    if (len(dados) != len(dados_saida)):     #caso do manchester que terá uma sequencia de bits maior
-        dados_repetidos = np.repeat(dados, 2)
-        plt.figure(figsize=(20, 2))
-        plt.yticks(np.arange(-1, 2))
-        ax = plt.gca()                        
-        ax.get_xaxis().set_visible(False)    #esconde eixo x
-        #plt.axhline(y=0, color="gray")
-        for i in range(0, len(dados_saida), 2):
-            plt.axvline(i, color="black", linestyle="--")
-            #plt.text(i+1, 1.1, dados_repetidos[i], ha = "center", va = "bottom")    
-        plt.axvline(len(dados_repetidos), color="black", linestyle="--")        #para traçar a linha vetical no último clock  
-        # Impressão em de etapas (formato digital)
-        plt.step(np.arange(len(dados_saida)+1), dados_saida + [dados_saida[(len(dados_saida))-1]], where='post', color=cor)
-        plt.title(titulo)
-        plt.show()
-    
-    else:
-        plt.figure(figsize=(20, 2))
-        plt.yticks(np.arange(-1, 2))         #eixo y (-1, 0, 1)
-        ax = plt.gca()                        
-        ax.get_xaxis().set_visible(False)    #esconde eixo x
-        for i in range(0, len(dados_saida), 1):      #esse for serve para colocar linha pontilhadas verticalmente e a sequencia de bits emitida acima do sinal
-            plt.axvline(i, color="black", linestyle="--")    #traça linhas pontilhadas verticalmente
-            #plt.text(i+0.5, 1.1, dados[i], ha = "center", va = "bottom") 
-        plt.axvline(len(dados_saida), color="black", linestyle="--")        #para traçar a linha vetical no último clock    
-        # Impressão em de etapas (formato digital)
-        plt.step(np.arange(0, len(dados_saida)+1), dados_saida + [dados_saida[(len(dados_saida))-1]], where='post', color=cor)
-        plt.title(titulo)
-        plt.show()
+def mostrar_grafico(dados, titulo, cor):
+  plt.figure(figsize=(20, 2))
+  plt.yticks(np.arange(-1, 2))
+  if titulo == "MANCHESTER":
+    plt.xticks(np.arange(0, (len(dados)/2)+1))
+    for i in range(0, int((len(dados)/2)), 1):
+      plt.axvline(i, color="black", linestyle="--")
+    plt.axvline(len(dados)/2, color="black", linestyle="--")
+    # Impressão em etapas (formato digital)
+    plt.step(np.arange(0, (len(dados)/2)+0.5, 0.5), dados + [dados[len(dados)-1]], where="post", color=cor)
+    plt.title(titulo)
+    plt.show()
+  else:
+    plt.xticks(np.arange(0, len(dados)+1))
+    for i in range(0, len(dados), 1):
+      plt.axvline(i, color="black", linestyle="--")
+    plt.axvline(len(dados), color="black", linestyle="--")
+    # Impressão em formato digital
+    plt.step(np.arange(0, len(dados)+1, 1), dados + [dados[(len(dados))-1]], where="post", color=cor)
+    plt.title(titulo)
+    plt.show()
